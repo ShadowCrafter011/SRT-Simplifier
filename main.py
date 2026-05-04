8caa751c713f8b5a59bf37b1a19804512823152c
Lukas <lkoe@bluewin.ch>
2026-05-04 18:16:39 +0000

    Added a font clean workflow

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from argparse import ArgumentParser
 import os
 import re
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument("clean_type", help="Either \"full\" or \"remove_font\"")
+    args = parser.parse_args()
+
     for file in os.listdir("data"):
         if file == ".keep":
             continue
@@ -14,26 +19,43 @@ def main():
             subtitle_data = srt_file.read()
 
         subtitles = subtitle_data.split("\n\n")
-        cleaned_subtitles = []
+        
+        if args.clean_type == "full":
+            full_clean(file, subtitles)
+        elif args.clean_type == "remove_font":
+            remove_font(file, subtitles)
 
-        for subtitle in subtitles:
-            subtitle_data = subtitle.split("\n")
+def full_clean(file, subtitles):
+    cleaned_subtitles = []
 
-            if len(subtitle_data) <= 2:
-                continue
+    for subtitle in subtitles:
+        subtitle_data = subtitle.split("\n")
 
-            subtitle_number, _, *content = subtitle_data
-            content = "\n".join(content)
+        if len(subtitle_data) <= 2:
+            continue
 
-            content = re.sub(r"<b><font [a-zA-Z0-9''=#]*>", "", content)
-            content = re.sub(r"</font></b>", "", content)
-            cleaned_subtitles.append(f"{subtitle_number} {content}")
+        subtitle_number, _, *content = subtitle_data
+        content = "\n".join(content)
 
-        cleaned_subtitles_string = "\n".join(cleaned_subtitles)
+        content = remove_font_regex(content)
+        cleaned_subtitles.append(f"{subtitle_number} {content}")
 
-        with open(os.path.join("data", f"{file.removesuffix(".srt")}-cleaned.srt"), "w") as cleaned_file:
-            cleaned_file.write(cleaned_subtitles_string)
+    cleaned_subtitles_string = "\n".join(cleaned_subtitles)
 
+    with open(os.path.join("data", f"{file.removesuffix(".srt")}-cleaned-full.srt"), "w") as cleaned_file:
+        cleaned_file.write(cleaned_subtitles_string)
+
+def remove_font(file, subtitles):
+    full_subtitles = "\n\n".join(subtitles)
+    cleaned_subtitles = remove_font_regex(full_subtitles)
+
+    with open(os.path.join("data", f"{file.removesuffix(".srt")}-cleaned-font.srt"), "w") as cleaned_file:
+        cleaned_file.write(cleaned_subtitles)
+
+def remove_font_regex(string):
+    string = re.sub(r"<b><font [a-zA-Z0-9''=#]*>", "", string)
+    return re.sub(r"</font></b>", "", string)
+    
 
 if __name__ == "__main__":
     main()
